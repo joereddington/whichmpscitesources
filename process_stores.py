4f12b9a71cae29a0eaa1726c895dba48b11cbb29
Joe Reddington <joe@joereddington.com>
2015-10-28 09:58:59 +0000

    The process script now populates a database and has a todo list inside. I also think I fixed a bug with the tweetdumper and names

--- process_stores.py
+++ process_stores.py
@@ -1,8 +1,18 @@
+
 import csv
 import re
+import os
 
+"""
+TODO:
 
-def process_file(filename, cutoff_date=None):
+Add the ratios
+Sort output by ratios
+Make HTML output
+Add links to html
+cut people who have less than X qualifying tweets
+
+def process_file(filename, database, cutoff_date=None):
         # So what do we want?
         total_tweets = 0
         tweets_with_numbers = 0
@@ -12,12 +22,19 @@ def process_file(filename, cutoff_date=None):
         f = open(filename, 'rt')
         try:
                 reader = csv.reader(f)
+                headings = next(reader, None)
                 for tweet in reader:
                         total_tweets += 1
                         if re.match(".* \\d+,*\\d* .*", tweet[2]):
                                 tweets_with_numbers += 1
                                 if "http" in tweet[2]:
                                         tweets_with_numbers_and_links += 1
+                database[
+                    headings[0]] = (
+                    headings[1],
+                    total_tweets,
+                    tweets_with_numbers,
+                    tweets_with_numbers_and_links)
         finally:
                 f.close()
         return (total_tweets,
@@ -25,4 +42,16 @@ def process_file(filename, cutoff_date=None):
                 tweets_with_numbers_and_links)
 
 
-print process_file('doug.csv')
+def populate_database():
+        database = {}
+        for f in os.listdir('testdata'):
+                process_file('testdata/'+f, database)
+        return database
+
+
+def produce_html(database):
+        # For each filename
+        for key, value in database.iteritems():
+                print "{} (@{}): {}, {}, {}".format(value[0], key, value[1], value[2], value[3])
+
+produce_html(populate_database())
--- tweet_dumper.py
+++ tweet_dumper.py
@@ -31,40 +31,50 @@ access_secret = config.get('twitter', 'access_secret')
 
 
 def get_all_handles(filename):
-    """Returns all of the twitter handles in the filename"""
-    file=open(filename)
-    screennames=[]
-    for rawline in file:
-        splitline=rawline.split('@')
-        screennames.insert(0,splitline[1])
-    return screennames
+        """Returns all of the twitter handles in the filename"""
+        file = open(filename)
+        screennames = []
+        for rawline in file:
+                splitline = rawline.split('@')
+                screennames.insert(0, splitline[1])
+        return screennames
+
 
 def get_all_tweets(screen_name):
-        # Twitter only allows access to a users most recent 3240 tweets with
-        # this method
+                # Twitter only allows access to a users most recent 3240 tweets with
+                # this method
 
-        # authorize twitter, initialize tweepy
+                # authorize twitter, initialize tweepy
         auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
         auth.set_access_token(access_key, access_secret)
-        api = tweepy.API(auth,wait_on_rate_limit=True)
+        api = tweepy.API(auth, wait_on_rate_limit=True)
 
         # initialize a list to hold all the tweepy Tweets
         alltweets = []
 
         # make initial request for most recent tweets (200 is the maximum
         # allowed count)
-	try:
-		new_tweets = api.user_timeline(screen_name=screen_name, count=200)
-	except tweepy.TweepError as e:
-	 	if "Not authorized." in  e.message:
-			print "The user {} appears to NOT exist".format(screen_name)
-			return
-
+        try:
+                new_tweets = api.user_timeline(
+                    screen_name=screen_name,
+                    count=200)
+        except tweepy.TweepError as e:
+                if "Not authorized." in e.message:
+                        print "The user {} appears to NOT exist".format(screen_name)
+                        return
+        user = api.get_user(screen_name)
+        top_line = (
+            user.screen_name,
+            user.name,
+            user.description,
+            user.followers_count,
+            user.statuses_count,
+            user.url)
         # save most recent tweets
         alltweets.extend(new_tweets)
-        #some people have NO tweets
-	if len(alltweets)==0:
-		return
+        # some people have NO tweets
+        if len(alltweets) == 0:
+                return
         # save the id of the oldest tweet less one
         oldest = alltweets[-1].id - 1
 
@@ -93,15 +103,15 @@ def get_all_tweets(screen_name):
             for tweet in alltweets]
 
         # write the csv
-        with open('%s_tweets.csv' % screen_name, 'wb') as f:
+        with open('%s_tweets.csv' % screen_name.strip(), 'wb') as f:
                 writer = csv.writer(f)
-                writer.writerow(["id", "created_at", "text"])
+                writer.writerow(top_line)
                 writer.writerows(outtweets)
 
         pass
 
 
 if __name__ == '__main__':
-	names=get_all_handles('mps.txt')
+        names = get_all_handles('mps.txt')
         for mp in names:
-            get_all_tweets(mp)
+                get_all_tweets(mp)
