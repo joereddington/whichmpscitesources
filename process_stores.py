493df9683c52be75923f7569f189dca9bff88ca8
Joseph Reddington <joe@joereddington.com>
2021-05-13 14:52:26 +0100

    Adding framwork for automatic tweets

--- process_stores.py
+++ process_stores.py
@@ -9,11 +9,25 @@ TODO:
 Add the ratios
 Sort output by ratios
 Make HTML output
+
+
+
 Add links to html
 add links to the set of tweets downloaded
 cut people who have less than X qualifying tweets
 """
 
+# from https://codereview.stackexchange.com/questions/41298/producing-ordinal-numbers 
+SUFFIXES = {1: 'st', 2: 'nd', 3: 'rd'}
+def ordinal(num):
+    # I'm checking for 10-20 because those are the digits that
+    # don't follow the normal counting scheme. 
+    if 10 <= num % 100 <= 20:
+        suffix = 'th'
+    else:
+        # the second parameter is a default.
+        suffix = SUFFIXES.get(num % 10, 'th')
+    return str(num) + suffix
 
 def process_file(filename, database, cutoff_date=None):
         # So what do we want?
@@ -64,14 +78,27 @@ def populate_database():
                 process_file(f, database)
         return database
 
-
-def produce_html(database):
-        # For each filename
+def produce_tweet(database):
         processed_mps=database.iteritems()
         processed_mps=sorted(processed_mps,key=lambda  k:k[1][4], reverse=True)
         rank=0
         for key, value in processed_mps:
                 rank+=1
  #               print "{} (@{}): {}, {}, {} {:.2f}%".format(value[0], key, value[1], value[2], value[3], value[4]*100)
-                print "<tr><td>{}</td><td>{} </td><td> ({}) </td><td> {} </td><td> {} </td><td> {} </td><td> {:.2f}% </td></tr>".format(rank,value[0], key, value[1], value[2], value[3], value[4]*100)
-produce_html(populate_database())
+                print "Congratulations to {} who ranked {} out of the 585 MPs (that I have data for) for sourcing the figures in their tweets. https://joereddington.com/rankingmps/ ".format(key, ordinal(rank))
+
+def produce_html(database):
+        # For each filename
+        processed_mps=database.iteritems()
+        processed_mps=sorted(processed_mps,key=lambda  k:k[1][4], reverse=True)
+        rank=0
+        f = open("list.txt", "w")
+        for key, value in processed_mps:
+                rank+=1
+                f.write("<tr><td>{}</td><td>{} </td><td> ({}) </td><td> {} </td><td> {} </td><td> {} </td><td> {:.2f}% </td></tr>".format(rank,value[0], key, value[1], value[2], value[3], value[4]*100))
+        f.close()
+
+database=populate_database()
+produce_html(database)
+produce_tweet(database)
+
--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 cd /home/joereddington/joereddington.com/mps_always
 python tweet_dumper.py
 mv *.csv full/
-python process_stores.py > list.txt
+python process_stores.py > tweetme.txt
 #tar -zcvf "$(date '+%y-%m-%d').tar.gz" full/
